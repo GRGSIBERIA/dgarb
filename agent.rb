86849edc8d244375e2fb91d9581750f21c0fc096
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-09-05 03:06:16 +0900

    disable openssl warning

--- agent.rb
+++ agent.rb
@@ -1,11 +1,18 @@
 #-*- encoding: utf-8
 require "mechanize"
+require "openssl"
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+I_KNOW_THAT_OPENSSL_VERIFY_PEER_EQUALS_VERIFY_NONE_IS_WRONG = nil
 
 module DW
+
   class Agent
     def initialize()
       @url = "https://danbooru.donmai.us/"
       @agent = Mechanize.new
     end
+
   end
+
 end
