b3c4949640962b55ab3057f2591795fa702117c5
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-09-06 01:26:11 +0900

    add addParam in tag

--- tag.rb
+++ tag.rb
@@ -12,6 +12,12 @@ module DW
       uri = @url + "tags.json?"
       uri += addParam(params, :limit)
       uri += addParam(params, :page)
+      uri += addParam(params, :search_name_matches)
+      uri += addParam(params, :search_category)
+      uri += addParam(params, :search_hide_empty)
+      uri += addParam(params, :search_order)
+      uri += addParam(params, :search_has_wiki)
+      uri += addParam(params, :search_name)
 
       getJSON(uri)
     end
