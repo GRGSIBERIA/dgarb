b922c65866e65bc679285f34935063ccf70c5d29
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-09-06 01:20:27 +0900

    add tag methods

--- tag.rb
+++ tag.rb
@@ -1,1 +1,19 @@
 #-*- encoding: utf-8
+
+require "./agent.rb"
+
+module DW
+  class Tag < Agent
+    def initialize()
+      super
+    end
+
+    def listing(params={})
+      uri = @url + "tags.json?"
+      uri += addParam(params, :limit)
+      uri += addParam(params, :page)
+
+      getJSON(uri)
+    end
+  end
+end
