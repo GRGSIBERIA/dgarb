6e07ab6c745519918533a4d2434ede2274f7fe58
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-09-06 01:40:49 +0900

    add tag aliases

---
+++ tag_alias.rb
@@ -0,0 +1,19 @@
+#-*- encoding: utf-8
+
+require "./agent.rb"
+
+module DW
+  class TagAlias < Agent
+    def initialize()
+      super
+    end
+
+    def listing(params={})
+      uri = @url + "tag_aliases.json?"
+      uri += addParams(params, :search_name_matches)
+      uri += addParams(params, :search_antecedent_name)
+      uri += addParams(params, :search_id)
+      getJSON(uri)
+    end
+  end
+end
