04d3a94b3d5e8a615d6be9c060cdb80b6cc6883a
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-09-10 01:21:43 +0900

    app.rbに統合

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 #-*- encoding: utf-8
 
-require "./request/danbooru.rb"
+require "./request/post.rb"
+require "./request/artist.rb"
+require "./request/pool.rb"
+require "./request/tag.rb"
+require "./request/tag_implication.rb"
+require "./request/related_tag.rb"
 
 puts DGrab::Post.show(1)
--- request/danbooru.rb
+++
@@ -1,8 +0,0 @@
-#-*- encoding: utf-8
-require "./request/post.rb"
-require "./request/artist.rb"
-require "./request/pool.rb"
-require "./request/tag.rb"
-require "./request/tag_implication.rb"
-require "./request/related_tag.rb"
-require "./request/helper.rb"
