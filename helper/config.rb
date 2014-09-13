905b5b25ffa6a912276b3a5b74d8024e8afe0f8f
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-09-13 21:38:46 +0900

    ベーシック認証が必要だったので，それに合わせて改変

--- helper/config.rb
+++ helper/config.rb
@@ -3,7 +3,8 @@
 require "./request/const.rb"
 
 require "yaml"
-require "pp"
+require "mechanize"
+require "base64"
 
 module DGrab
 
@@ -11,7 +12,7 @@ module DGrab
 
     # 設定ファイルクラス
     class Config
-      attr_accessor :db_dir, :img_dir, :username, :password
+      attr_accessor :db_dir, :img_dir, :username
 
       def initialize()
 
@@ -23,41 +24,38 @@ module DGrab
 
         @db_dir = yaml["db_dir"]
         @img_dir = yaml["img_dir"]
-        @username = yaml["username"].to_s
-        @password = yaml["password"].to_s
         @provider = yaml["provider"]
+        @api_key = yaml["api_key"]
+        @username = yaml["username"]
         @db_dir ||= "./"
         @img_dir ||= "./img"
-        @password ||= ""
-        @username ||= ""
         @provider ||= "Danbooru"
+        @api_key ||= ""
+        @username ||= ""
 
-        if not @username.empty? and not @password.empty? then
-          # ここで一度Providerでログイン処理する
-          if @provider == "Danbooru" then
-            page = DGrab::Request::AGENT.get("https://danbooru.donmai.us/session/new")
-            result = page.form_with(:class => "simple_form") {|form|
-              form.name = @username.toutf8
-              form.password = @password.toutf8
-              puts form.name, form.password
-            }.submit
-
-            if result.body.include?("Password was incorrect.") then
-              raise ArgumentError, "ログインに失敗しました"
-            end
-          end
+        if not @api_key.empty? and not @username.empty? then
+          @auth = Base64.encode64("#{@username}:#{@api_key}") + "=="
+          @auth.encode!("utf-8")
         end
+        @auth ||= ""
+      end
+
+      def basic_auth
+        "Basic #{@auth}"
       end
 
       def Config.init(yaml_path)
         CONFIG.init(yaml_path)
       end
+
     end
 
     # 設定重要なのでグローバルに
     CONFIG = Config.new
 
     # コンフィグを読み込む
+    # @param [String] path 設定のYAMLファイルのパス
+    # @return [DGrab::Helper::Config] コンフィグのインスタンス
     def import_config(path=nil)
       path ||= "./config.yaml"
       if not File.exist?(path) then
