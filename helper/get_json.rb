224c7234644592a5054f832d6868cd702644863b
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-09-14 01:34:57 +0900

    簡単なメソッド追加

--- helper/get_json.rb
+++ helper/get_json.rb
@@ -2,8 +2,13 @@
 
 module DGrab
   module Helper
-    def get_json()
-
+    # @params [Class] api_method 呼び出したいリクエストのクラス名
+    # @params [Symbol] call_method メソッド名のシンボル
+    # @params [Hash] params posts.jsonに対するパラメータ
+    def get_json(api_type, call_method, params={})
+      api_type.method(call_method).call(params)
     end
+
+    module_function :get_json
   end
 end
