105065d352310e3cf8a1f323d9a64d56bda5f006
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-09-14 15:37:41 +0900

    モジュール名のフルパスを付けないと読み込めない問題が発生

--- request/const.rb
+++ request/const.rb
@@ -10,18 +10,16 @@ I_KNOW_THAT_OPENSSL_VERIFY_PEER_EQUALS_VERIFY_NONE_IS_WRONG = nil
 
 module DGrab
   module Request
-    include DGrab::Helper
 
     DANBOORU_URL = "https://danbooru.donmai.us/"
     AGENT = Mechanize.new
     AGENT.user_agent_alias = "Windows Mozilla"
 
-    if CONFIG.authorize then
-      puts "test"
+    if DGrab::Helper::CONFIG.authorize then
       AGENT.cookie_jar.add(
         DANBOORU_URL[0..-2],
         Mechanize::Cookie.new(
-          CONFIG.username, DANBOORU_URL[0..2], { api_key: CONFIG.api_key }
+          DGrab::Helper::CONFIG.username, DANBOORU_URL[0..2], { api_key: DGrab::Helper::CONFIG.api_key }
           ))
     end
   end
